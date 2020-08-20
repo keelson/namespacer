c4ef1d54fafb3061f618e69858f127a791cf482b
Boris Ruppert <bobo@sideburns.de>
2020-08-20 17:31:10 +0200

    initial workin version

--- container/namespacer.sh
+++ container/namespacer.sh
@@ -1,26 +1,51 @@
 #!/usr/bin/env bash
-set -e  -x
-source /shell_lib.sh
 
-function __config__() {
+ARRAY_COUNT=`jq -r '. | length-1' ${BINDING_CONTEXT_PATH}`
+
+if [[ $1 == "--config" ]] ; then
   cat <<EOF
-    configVersion: v1
-    kubernetes:
-    - name: OnCreateDeleteNamespace
-      apiVersion: v1
-      kind: Namespace
-      executeHookOnEvent:
-      - Added
+configVersion: v1
+kubernetes:
+- name: OnCreateDeleteNamespace
+  apiVersion: v1
+  kind: Namespace
+  executeHookOnEvent:
+  - Added
+  - Deleted
+- name: OnModifiedNamespace
+  apiVersion: v1
+  kind: Namespace
+  executeHookOnEvent:
+  - Modified
+  jqFilter: ".metadata.labels"
 EOF
-}
+else
+  # ignore Synchronization for simplicity
+  type=$(jq -r '.[0].type' ${BINDING_CONTEXT_PATH})
+  if [[ "${type}" == "Synchronization" ]] ; then
+    echo Got Synchronization event
+    exit 0
+  fi
 
+  for IND in `seq 0 ${ARRAY_COUNT}`
+  do
+    bindingName=`jq -r ".[${IND}].binding" ${BINDING_CONTEXT_PATH}`
+    resourceEvent=`jq -r ".[${IND}].watchEvent" ${BINDING_CONTEXT_PATH}`
+    resourceName=`jq -r ".[${IND}].object.metadata.name" ${BINDING_CONTEXT_PATH}`
 
-
-function __main__() {
-  namespace=$(jq -r .[0].object.metadata.name ${BINDING_CONTEXT_PATH})
-  echo "HUUUURZ namespace ${namespace} added"
-  #kubectl label ns ${namespace} ivebeenwatchingyou=alalalalong
-}
-
-hook::run "$@"
-
+    if [[ "${bindingName}" == "OnModifiedNamespace" ]] ; then
+      echo "Namespace ${resourceName} labels were modified"
+    else
+      if [[ "${resourceEvent}" == "Added" ]] ; then
+        echo "Namespace ${resourceName} was created"
+        if [[ "${resourceName}" == ${IGNORE_NAMESPACE} ]]; then 
+          echo "Namespace ${resourceName} is on the ignore list, skipping"
+        else 
+          kubectl label namespace ${resourceName} ${LABEL}
+        fi
+      else
+        echo "Namespace ${resourceName} was deleted"
+      fi
+    fi
+  done
+fi
