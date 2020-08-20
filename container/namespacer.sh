25da71503c6b48d3cc18c1bd1379566e8cf2ab97
Boris Ruppert <bobo@sideburns.de>
2020-08-20 16:08:43 +0200

    save work, not working

--- container/namespacer.sh
+++ container/namespacer.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
-set -e 
+set -e  -x
+source /shell_lib.sh
 
-if [[ $1 == "--config" ]] ; then
+function __config__() {
   cat <<EOF
-configVersion: v1
-kubernetes:
-- apiVersion: v1
-  kind: Namespace
-  executeHookOnEvent: ["Added"]
+    configVersion: v1
+    kubernetes:
+    - name: OnCreateDeleteNamespace
+      apiVersion: v1
+      kind: Namespace
+      executeHookOnEvent:
+      - Added
 EOF
-else
-  namespace=$(jq -r .[0].object.metadata.name $BINDING_CONTEXT_PATH)
-  echo "namespace '${namespace}' added"
-fi
+}
+
+
+
+function __main__() {
+  namespace=$(jq -r .[0].object.metadata.name ${BINDING_CONTEXT_PATH})
+  echo "HUUUURZ namespace ${namespace} added"
+  #kubectl label ns ${namespace} ivebeenwatchingyou=alalalalong
+}
+
+hook::run "$@"
+
